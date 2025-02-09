38b92d6eb0ff3190d2c9b6070865c82e42fe5a42
Vivek Jain <vivek@vivekja.in>
2025-02-09 10:57:19 -0800

    Add Github action

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+    // Allow Github Action to override base
+    base: process.env.OVERRIDE_BASE || undefined,
     plugins: [react()]
 })
